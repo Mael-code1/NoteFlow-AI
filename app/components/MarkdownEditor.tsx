c8a812e9ee027117b63764a82c45b5473d33a1b6
Mael-code1 <mael_code@outlook.com>
2024-10-09 18:49:11 -0600

    correcion de la ruta protegida

--- app/components/MarkdownEditor.tsx
+++ app/components/MarkdownEditor.tsx
@@ -46,7 +46,7 @@ const MarkdownEditor = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row p-4 space-y-4 md:space-x-4">
+    <div className="flex flex-col h-auto w-full">
       <div className="w-full md:w-1/2 mb-2 space-x-2">
         <button
           className="p-2 bg-blue-500 text-white rounded-md"
@@ -79,51 +79,54 @@ const MarkdownEditor = () => {
           Código
         </button>
       </div>
-
-      <textarea
-        id="markdown-editor"
-        className="w-full md:w-1/2 p-2 border border-gray-300 rounded-md text-black"
-        value={markdownText}
-        onChange={handleInputChange}
-        placeholder="Escribe tu Markdown aquí..."
-        rows={20}
-      />
-
-      <div className="w-full md:w-1/2 p-2 border border-gray-300 rounded-md bg-gray-50 text-black">
-        <ReactMarkdown
-          children={markdownText}
-          remarkPlugins={[remarkGfm]}
-          components={{
-            h1: ({ children }) => (
-              <h1 className="text-3xl font-bold text-blue-500">{children}</h1>
-            ),
-            h2: ({ children }) => (
-              <h2 className="text-2xl font-semibold text-blue-400">
-                {children}
-              </h2>
-            ),
-            h3: ({ children }) => (
-              <h3 className="text-xl font-medium text-blue-300">{children}</h3>
-            ),
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  style={dracula}
-                  language={match[1]}
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-          }}
+      <div className="flex flex-row gap-4">
+        <textarea
+          id="markdown-editor"
+          className="w-full md:w-1/2 p-2 border border-gray-300 rounded-md text-black"
+          value={markdownText}
+          onChange={handleInputChange}
+          placeholder="Escribe tu Markdown aquí..."
+          rows={20}
         />
+
+        <div className="w-full md:w-1/2 p-2 border  rounded-md bg-slate-800 text-white">
+          <ReactMarkdown
+            children={markdownText}
+            remarkPlugins={[remarkGfm]}
+            components={{
+              h1: ({ children }) => (
+                <h1 className="text-3xl font-bold text-blue-500">{children}</h1>
+              ),
+              h2: ({ children }) => (
+                <h2 className="text-2xl font-semibold text-blue-400">
+                  {children}
+                </h2>
+              ),
+              h3: ({ children }) => (
+                <h3 className="text-xl font-medium text-blue-300">
+                  {children}
+                </h3>
+              ),
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    style={dracula}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
--- middleware.ts
+++ middleware.ts
@@ -24,5 +24,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/home", "/editor"],
+  matcher: ["/home", "/home/editor"],
 };
